84d52ebc6791fb0b8ac42beb7ca96b6866bc4bf4
Paxo182 <pablo95informatica@gmail.com>
2021-06-22 16:49:26 -0400

    4

--- server/mongo.php
+++ server/mongo.php
@@ -1,6 +1,7 @@
 <?php
 
 $_MONGO = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+$_DB    = 'db_acceso';
 
 //echo __select($m, 'db_acceso.usuario', ['usr' => 'admin','psw' => '123' ] ,[] );
 
@@ -32,12 +33,12 @@ function __select($_CONN, $_TABLA, $_FILTRO , $_OPCIONES )
 }
 
 ///crea la collection
-function __collection($_CONN,$_ENTIDAD)
+function __collection($_CONN,$_DB,$_ENTIDAD)
 {
     $command = new MongoDB\Driver\Command(['listCollections' => 1, 'filter' => ['name' =>  $_ENTIDAD ] ] );
 
     //var_dump($command);
-    $result = $_CONN->executeCommand('db_acceso', $command )->toArray();
+    $result = $_CONN->executeCommand($_DB, $command )->toArray();
     //var_dump($result);
 
     if (false == empty($result))
@@ -47,7 +48,7 @@ function __collection($_CONN,$_ENTIDAD)
     else
     {
         $command = new MongoDB\Driver\Command(["create" => $_ENTIDAD]);
-        $cursor = $_CONN->executeCommand("db_acceso", $command);
+        $cursor = $_CONN->executeCommand($_DB, $command);
         $response = $cursor->toArray()[0];
     }
 }
