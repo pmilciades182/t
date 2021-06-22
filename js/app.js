860520ce087c449987051c2b59b597a465d2cef1
paxo182 <pablo95informatica@gmail.com>
2021-06-21 22:38:55 -0400

    cosas

---
+++ js/app.js
@@ -0,0 +1,28 @@
+function e__put_th(e,f)
+{
+    let k = document.createElement("th");
+    let h = document.createElement("th");
+    f[0].appendChild(k);
+    f[0].appendChild(h);
+    e.forEach(function (m) 
+    {
+        k = document.createElement("th");
+        m = capitalizeTheFirstLetterOfEachWord(m);
+        k.innerHTML = m;
+        console.log(f);
+        f[0].appendChild(k);
+    });
+
+}
+
+
+
+function capitalizeTheFirstLetterOfEachWord(words) {
+    words = words.replace('_',' ');
+    var separateWord = words.toLowerCase().split(' ');
+    for (var i = 0; i < separateWord.length; i++) {
+       separateWord[i] = separateWord[i].charAt(0).toUpperCase() +
+       separateWord[i].substring(1);
+    }
+    return separateWord.join(' ');
+ }
