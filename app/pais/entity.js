7809a612871380541b252b1a4e4b25787a290477
paxo182 <pablo95informatica@gmail.com>
2021-07-28 19:54:54 -0400

    entity

---
+++ app/pais/entity.js
@@ -0,0 +1,21 @@
+
+    /////globales de entidad
+
+    var entity = 'pais';
+
+    var page   = 1;
+
+    var cols_grid = [
+        'id',
+        'descripcion'
+    ];
+
+    var cols_form = [
+        'id',
+        'descripcion'
+    ];
+
+    var cols_search = [
+        'descripcion'
+   
+    ];
---
+++ app/persona/entity.js
@@ -0,0 +1,42 @@
+ /////globales de entidad
+
+ var entity = 'persona';
+ 
+//// pagina inicial
+ var page   = 1;
+
+ var cols_grid = [
+     'id',
+     'nombre',
+     'apellido',
+     'telefono',
+     'email',
+     'fecha_nacimiento',
+     'nacionalidad',
+     'tipo_documento',
+     'nro_documento',
+     'direccion',
+     'pais',
+     'ciudad'
+ ];
+
+ var cols_form = [
+     'id',
+     'nombre',
+     'apellido',
+     'telefono',
+     'email',
+     'fecha_nacimiento',
+     'nacionalidad',
+     'tipo_documento',
+     'nro_documento',
+     'direccion',
+     'pais',
+     'ciudad'
+ ];
+
+ var cols_search = [
+     'nombre',
+     'apellido',
+     'nro_documento'
+ ];
